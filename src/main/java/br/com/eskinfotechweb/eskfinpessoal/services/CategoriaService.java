3990a1445a85abbc2ea5c3d109649c947be52d36
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-19 12:23:52 -0300

    Implementando endspoint GET categorias e categorias/{id}

--- src/main/java/br/com/eskinfotechweb/eskfinpessoal/services/CategoriaService.java
+++ src/main/java/br/com/eskinfotechweb/eskfinpessoal/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package br.com.eskinfotechweb.eskfinpessoal.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
+	}
+	
+	public Categoria findById(Long id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElse(null);
 	}
 }
