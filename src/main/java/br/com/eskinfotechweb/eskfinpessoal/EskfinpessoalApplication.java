e8eef9aae774bbc408abfc675f195ef90489537c
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-19 08:46:08 -0300

    Projeto criado

---
+++ src/main/java/br/com/eskinfotechweb/eskfinpessoal/EskfinpessoalApplication.java
@@ -0,0 +1,13 @@
+package br.com.eskinfotechweb.eskfinpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskfinpessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskfinpessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eskinfotechweb/eskfinpessoal/EskfinpessoalApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.eskinfotechweb.eskfinpessoal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EskfinpessoalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
