611f17d6a94ae3159471274deed84f96625dfa5b
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-25 16:22:35 -0300

    Refatorando a implementação de segurança com OAuth 2 e Password Flow

--- src/main/java/br/com/eskinfotechweb/eskfinpessoal/config/OAuthSecurityConfig.java
+++ src/main/java/br/com/eskinfotechweb/eskfinpessoal/config/OAuthSecurityConfig.java
@@ -3,20 +3,14 @@ package br.com.eskinfotechweb.eskfinpessoal.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableAuthorizationServer
-@EnableResourceServer
-public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
+public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
     @Override
