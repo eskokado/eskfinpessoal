a93a21847cd8b05a8acaf862dfda02fae659f18c
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-26 09:57:34 -0300

    Refatorando - Movendo o usuário para o banco de dados

--- src/main/java/br/com/eskinfotechweb/eskfinpessoal/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/eskinfotechweb/eskfinpessoal/config/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -23,6 +24,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
@@ -40,6 +44,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.tokenStore(tokenStore())
 			.accessTokenConverter(accessTokenConverter())
 			.reuseRefreshTokens(false)
+			.userDetailsService(this.userDetailsService)
 			.authenticationManager(authenticationManager);
 	}
 
--- src/main/java/br/com/eskinfotechweb/eskfinpessoal/config/ResourceServerConfig.java
+++ src/main/java/br/com/eskinfotechweb/eskfinpessoal/config/ResourceServerConfig.java
@@ -1,35 +1,30 @@
 package br.com.eskinfotechweb.eskfinpessoal.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
-@EnableWebSecurity
 @EnableResourceServer
+//@EnableWebSecurity
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	@Autowired
-	private UserDetailsService userDetailsService;
+//	@Autowired
+//	private UserDetailsService userDetailsService;
 	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+//	@Autowired
+//	private PasswordEncoder passwordEncoder;
 		
-	@Autowired
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//	@Autowired
+//	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 //		auth.inMemoryAuthentication()
 //			.withUser("eskokado").password("3sk0k@d00").roles("ADMIN");
-		auth.userDetailsService(userDetailsService)
-			.passwordEncoder(passwordEncoder);
-	}
+//		auth.userDetailsService(userDetailsService)
+//			.passwordEncoder(passwordEncoder);
+//	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
