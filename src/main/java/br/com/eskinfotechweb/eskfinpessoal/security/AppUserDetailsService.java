6275317cac1b6e0e70fb1bde7e2a570b5d8be27e
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-27 08:49:20 -0300

    Nome do usuário no token JWT

--- src/main/java/br/com/eskinfotechweb/eskfinpessoal/security/AppUserDetailsService.java
+++ src/main/java/br/com/eskinfotechweb/eskfinpessoal/security/AppUserDetailsService.java
@@ -7,7 +7,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -25,7 +24,8 @@ public class AppUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Usuario usuario = usuarioService.findByEmail(email);
-		return new User(email, usuario.getSenha(), getPermissoes(usuario));
+		// return new User(email, usuario.getSenha(), getPermissoes(usuario));
+		return new UsuarioSistema(usuario, getPermissoes(usuario));
 	}
 
 	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
